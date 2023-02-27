510b9b596b2e1b28b3ecc110b64e95d2b5d87b89
syu_asus_Note <syukunt@icloud.com>
2023-02-27 19:43:54 +0900

    【Nuxt.jsビギナーズガイド】Chapter1, Chapter2.14非同期コンテンツのSSRまで

---
+++ Chapter2/my-first-nuxt-app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	async mounted() {
+		const reaponse = await this.$axios.$get('https://qiita.com/api/v2/tags?page=1&per_page=20&sort=count HTTP/1.1');
+		const JSONData = JSON.stringify(reaponse, true, ' ');
+		console.log('Qiita API Response: ', JSONData);
+	},
+}
+</script>
---
+++ Chapter2/my-first-nuxt-app/pages/users/_id.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>
+            UserID: {{ userId }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                userId: this.$route.params.id
+            }
+        }
+    }
+</script>
