46aebf35850c5534a2468df07f94ea23756155c3
syu <syukunt@icloud.com>
2023-03-01 21:22:37 +0900

    【Nuxt.jsビギナーズガイド】4.28の実装

---
+++ Chapter4/app/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <el-menu mode="horizontal" :router="true">
+        <el-menu-item index="1" style="pointer-events: none;">Nuxt Diary Application</el-menu-item>
+        <el-menu-item index="2" :router="{path: '/posts/'}">投稿一覧</el-menu-item>
+        <no-ssr>
+            <el-menu-item idex="4" style="floot: right;" :route="{path: '/'}">ログイン</el-menu-item>
+        </no-ssr>
+        <el-menu-item index="5" style="float: right;" :route="{path: '/posts/new'}">新規投稿</el-menu-item>
+    </el-menu>
+</template>
+
---
+++ Chapter4/app/pages/posts/index.vue
@@ -0,0 +1,94 @@
+<template>
+    <section class="container posts-page">
+        <el-card>
+            <div slot="header" class="clearfix">
+                <span>新規投稿</span>
+            </div>
+            <el-table :data="showPosts" style="width: 100%;" class="table">
+                <el-table-column prop="title" label="タイトル"></el-table-column>
+                <el-table-column prop="user.id" label="投稿者" width="180"></el-table-column>
+                <el-table-column prop="created_at" label="投稿日" width="240"></el-table-column>
+            </el-table>
+        </el-card>
+    </section>
+</template>
+
+<script>
+export default {
+    computed: {
+        showPosts() {
+            return [
+                {
+                    id:"001",
+                    title: "how to develop nuxt.js",
+                    body: "nuxt.js is a framework for vue.js",
+                    created_at: "2019-01-01",
+                    user: {
+                        id: "test",
+                    },
+                },
+                {
+                    id:"002",
+                    title: "how to develop nuxt.js",
+                    body: "nuxt.js is a framework for vue.js",
+                    created_at: "2019-01-01",
+                    user: {
+                        id: "test",
+                    },
+                },
+                {
+                    id:"003",
+                    title: "how to develop nuxt.js",
+                    body: "nuxt.js is a framework for vue.js",
+                    created_at: "2019-01-01",
+                    user: {
+                        id: "test",
+                    },
+                },
+                {
+                    id:"004",
+                    title: "how to develop nuxt.js",
+                    body: "nuxt.js is a framework for vue.js",
+                    created_at: "2019-01-01",
+                    user: {
+                        id: "test",
+                    },
+                },
+                {
+                    id:"005",
+                    title: "how to develop nuxt.js",
+                    body: "nuxt.js is a framework for vue.js",
+                    created_at: "2019-01-01",
+                    user: {
+                        id: "test",
+                    },
+                },
+                {
+                    id:"006",
+                    title: "how to develop nuxt.js",
+                    body: "nuxt.js is a framework for vue.js",
+                    created_at: "2019-01-01",
+                    user: {
+                        id: "test",
+                    },
+                },
+                {
+                    id:"007",
+                    title: "how to develop nuxt.js",
+                    body: "nuxt.js is a framework for vue.js",
+                    created_at: "2019-01-01",
+                    user: {
+                        id: "test",
+                    },
+                },
+            ];
+        },
+    },
+}
+</script>
+
+<style>
+.posts-page .el-table__row{
+    cursor: pointer;
+}
+</style>
